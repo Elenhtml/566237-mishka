f985979cea5df2582997f49c0df0f74f78261729
Elenhtml <bagslife@yandex.ru>
2018-02-15 10:57:28 +0300

    Added popup-function in js

--- source/js/common.js
+++ source/js/common.js
@@ -2,7 +2,7 @@ var navMain = document.querySelector(".main-nav");
 var navButtonMenu = document.querySelector(".button-menu");
 
 navMain.classList.remove("main-nav--nojs");
-navButtonMenu.addEventListener("click", function() {
+navButtonMenu.addEventListener("click", function () {
   if (navMain.classList.contains("main-nav--closed")) {
       navMain.classList.remove("main-nav--closed");
       navMain.classList.add("main-nav--opened");
@@ -13,40 +13,48 @@ navButtonMenu.addEventListener("click", function() {
 });
 
 var buttonOrder = document.querySelector(".week-good__button-order");
-var modalMessage = document.querySelector(".modal-order");
-if (modalMessage) {
+if (buttonOrder) {
+  var modalMessage = document.querySelector(".modal-order");
+  var close = document.querySelector(".modal-overlay");
   buttonOrder.addEventListener("click", function (evt) {
     evt.preventDefault();
+    close.classList.add("modal-order-show"); 
     modalMessage.classList.add("modal-order-show");
+  });
+
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalMessage.classList.remove("modal-show");
+    close.classList.remove("modal-order-show");
   });
 
   window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       if (modalMessage.classList.contains("modal-order-show")) {
         modalMap.classList.remove("modal-order-show");
+        close.classList.remove("modal-order-show"); 
       }
     }
   });
 }
 
 var buttonsBuy = document.querySelectorAll(".button-buy");
-var modalMessage = document.querySelector(".modal-order");
-if (modalMessage) {
-  buttons_buy.forEach(function(button) {
-    button.addEventListener("click", function(evt) {
+if (buttonsBuy) {
+  var modalMessages = document.querySelector(".modal-order");
+  var close = document.querySelector(".modal-overlay");
+  buttonsBuy.forEach(function(button) {
+    button.addEventListener("click", function (evt) {
       evt.preventDefault();
-      modalMessage.classList.add("modal-order-show");
+      modalMessages.classList.add("modal-order-show");
+      close.classList.add("modal-order-show"); 
     });
-  });
-
-  modalMessage_close.addEventListener("click", function() {
-    modalMessage.classList.remove("modal-order-show");
   });
 
   window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
-      if (modalMessage.classList.contains("modal-order-show")) {
-        modalMessage.classList.remove("modal-order-show");
+      if (modalMessages.classList.contains("modal-order-show")) {
+        modalMessages.classList.remove("modal-order-show");
+        close.classList.remove("modal-order-show"); 
       }
     }
   });
