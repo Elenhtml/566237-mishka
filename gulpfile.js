0a799144303e920d151871d23827f6f2fc3eb232
Elenhtml <bagslife@yandex.ru>
2018-02-18 20:59:51 +0300

    Improved files after check

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ var server = require("browser-sync").create();
 
 gulp.task("copy", function()  {
   return gulp.src([
-    "source/fonts/*.{woff, woff2}",
+    "source/fonts/*.{woff,woff2}",
     "source/img/**",
     "source/js/**"
   ], {
